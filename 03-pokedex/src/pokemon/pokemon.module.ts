371de6181d2aa0bb7f50cce057d467a4b0fe26c2
Tobias <tobiasvega1210@gmail.com>
2025-03-28 23:11:14 -0300

    Se finalizó la seccion 7

--- 03-pokedex/src/pokemon/pokemon.module.ts
+++ 03-pokedex/src/pokemon/pokemon.module.ts
@@ -13,6 +13,6 @@ import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
       schema: PokemonSchema,
     }])
   ],
-  exports: [PokemonService]
+  exports: [MongooseModule]
 })
 export class PokemonModule {}
