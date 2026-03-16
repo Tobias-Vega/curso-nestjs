bc23c54788d7ef05bc52c75cae399aeddc0384b8
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-16 13:19:14 -0300

    Tipos de datos - continuación

--- 01-TypeScript-intro/src/bases/01-types.ts
+++ 01-TypeScript-intro/src/bases/01-types.ts
@@ -1,7 +1,22 @@
 
 
-export const name = 'Tobías';
+export let name: string = 'Tobías';
 export const age: number = 20;
 export const isValid: boolean = true;
 
 console.log({isValid});
+
+name = 'Meliisa';
+
+export const templateString = ` Esto es un string
+multilínea
+que puede tener
+" dobles
+'simples
+inyectar valores ${name}
+expresiones ${1 + 1}
+números: ${age}
+booleanos: ${isValid}
+`
+
+console.log(templateString);
