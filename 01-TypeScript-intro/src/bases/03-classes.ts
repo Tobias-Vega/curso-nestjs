91c9249b14eda83167ca0e2a0543e1c4f07ed738
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-16 13:37:21 -0300

    Getters, métodos y THIS

--- 01-TypeScript-intro/src/bases/03-classes.ts
+++ 01-TypeScript-intro/src/bases/03-classes.ts
@@ -2,12 +2,27 @@
 
 export class Pokemon {
 
+  get imageUrl(): string {
+    return `httpS://pokemon.com/${this.id}.jpg`;
+  }
+
   constructor(
     public readonly id: number,
-    public name: string
+    public name: string,
   ) {}
+
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!!`);
+  }
+
+  speak() {
+    console.log(`${this.name}, ${this.name}`);
+  }
 
 }
 
 export const charmander = new Pokemon(4, 'Charmander');
+
+console.log(charmander.scream());
+
 
