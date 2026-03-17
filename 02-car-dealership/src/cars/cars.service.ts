476cb1b768cd9bc2690b04fa6e1b099f13c31eba
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-17 13:41:32 -0300

    Inyección de dependencias

--- 02-car-dealership/src/cars/cars.service.ts
+++ 02-car-dealership/src/cars/cars.service.ts
@@ -14,9 +14,19 @@ export class CarsService {
       model: 'Civic',
     },
     {
-      id: 1,
+      id: 3,
       brand: 'Toyota',
       model: 'Cherokee',
     },
   ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+
+    return car;
+  }
 }
