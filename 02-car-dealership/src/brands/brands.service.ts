37126452ba14c997408358a0bba564140d6474b0
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-18 16:50:58 -0300

    Preparar servicios para insertar SEED

--- 02-car-dealership/src/brands/brands.service.ts
+++ 02-car-dealership/src/brands/brands.service.ts
@@ -59,4 +59,8 @@ export class BrandsService {
 
     this.brands = this.brands.filter((brand) => brand.id !== id);
   }
+
+  fillCarsWithSeedData(brands: Brand[]) {
+    this.brands = brands;
+  }
 }
