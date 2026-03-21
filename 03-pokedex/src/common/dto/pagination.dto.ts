391489d85a1729e9e1160781c1fc06fc5102cfba
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-21 00:36:54 -0300

    Transform DTOs - Fin sección 8

--- 03-pokedex/src/common/dto/pagination.dto.ts
+++ 03-pokedex/src/common/dto/pagination.dto.ts
@@ -3,9 +3,9 @@ import { IsOptional, IsPositive, Min } from 'class-validator';
 export class PaginationDto {
   @IsOptional()
   @Min(1)
-  limit: number;
+  limit?: number;
 
   @IsOptional()
   @IsPositive()
-  offset: number;
+  offset?: number;
 }
--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -9,6 +9,7 @@ import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -28,8 +29,17 @@ export class PokemonService {
     }
   }
 
-  async findAll() {
-    return await this.pokemonModel.find().limit(5).skip(5);
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.pokemonModel
+      .find()
+      .limit(limit)
+      .skip(offset)
+      .sort({
+        no: 1,
+      })
+      .select('-__v');
   }
 
   async findOne(term: string) {
