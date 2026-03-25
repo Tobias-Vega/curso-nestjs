582df0e96b923d05d5031a11544ef2a27a36136c
Tobias Vega <tobiasvega1210@gmail.com>
2026-03-25 11:20:26 -0300

    Create Product DTO

--- 04-teslo-shop/src/products/dto/create-product.dto.ts
+++ 04-teslo-shop/src/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({
+    each: true,
+  })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
