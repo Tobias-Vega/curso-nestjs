c7f35a02601a294e5916f6d3ba6f7ac12e62d673
Tobias Vega <tobiasvega1210@gmail.com>
2026-04-13 12:59:52 -0300

    Guardar imagen en filesystem

--- 04-teslo-shop/src/files/files.controller.ts
+++ 04-teslo-shop/src/files/files.controller.ts
@@ -8,6 +8,7 @@ import {
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
@@ -16,6 +17,10 @@ export class FilesController {
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileFilter,
+      // limits: { fileSize: 1000 },
+      storage: diskStorage({
+        destination: './static/uploads',
+      }),
     }),
   )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
