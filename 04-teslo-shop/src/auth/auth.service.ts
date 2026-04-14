c838bca357a9da17adfbb726b963e2737e75bf42
Tobias Vega <tobiasvega1210@gmail.com>
2026-04-14 13:48:04 -0300

    Cambiar el email por el id en el payload

--- 04-teslo-shop/src/auth/auth.service.ts
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -33,7 +33,7 @@ export class AuthService {
 
       return {
         ...user,
-        token: this.getJwtToken({ email: user.email }),
+        token: this.getJwtToken({ id: user.id }),
       };
     } catch (error) {
       this.handleDbErrors(error);
@@ -45,7 +45,7 @@ export class AuthService {
 
     const user = await this.userRespository.findOne({
       where: { email },
-      select: { email: true, password: true },
+      select: { email: true, password: true, id: true },
     });
 
     if (!user) {
@@ -58,7 +58,7 @@ export class AuthService {
 
     return {
       ...user,
-      token: this.getJwtToken({ email: user.email }),
+      token: this.getJwtToken({ id: user.id }),
     };
   }
 
